1ee82842e5a230b7701a83df436520cfaaf90832
SukkaW <isukkaw@gmail.com>
2025-09-18 06:52:40 +0800

    refactor: svg fetching

--- index.ts
+++ index.ts
@@ -12,7 +12,8 @@ baseUrl.searchParams.set('hide_border', 'true');
 baseUrl.searchParams.set('include_all_commits', 'true');
 baseUrl.searchParams.set('hide_title', 'true');
 baseUrl.searchParams.set('count_private', 'true');
-baseUrl.searchParams.set('show', 'reviews' /** reviews,discussions_started,discussions_answered */);
+baseUrl.searchParams.set('rank_icon', 'percentile');
+baseUrl.searchParams.set('show', 'reviews,prs_merged' /** reviews,discussions_started,discussions_answered */);
 
 const lightUrl = new URL(baseUrl);
 lightUrl.searchParams.set('icon_color', '586069');
@@ -25,17 +26,17 @@ darkUrl.searchParams.set('bg_color', '1f2228');
 darkUrl.searchParams.set('text_color', '8d939d');
 
 async function fetchSvg(url: URL) {
-  const random = Math.random().toString(36).slice(2);
-  url.searchParams.set('_cache_busting', random);
+  // const random = Math.random().toString(36).slice(2);
+  // url.searchParams.set('_cache_busting', random);
 
   const svg = await fetch(url, { signal: AbortSignal.timeout(10 * 1000) })
     .then(r => r.text())
     .catch(e => {
-      console.log('[GitHub README Stats Error]', url.href, e);
+      console.log('[GitHub README Stats Error] fetch failed', url.href, e);
       throw e;
     });
 
-  if (svg.includes('went wrong') || svg.includes('file an issue')) {
+  if (svg.includes('went wrong') || svg.includes('file an issue') || svg.includes('rate limit')) {
     console.log('[GitHub README Stats Error] Backend issue', url.href, svg);
     throw new Error('Failed to fetch');
   } else if (svg.includes('>0</text>')) {
